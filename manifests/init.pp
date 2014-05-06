5ded69950da4e8e078712e5086b3012d1e6acb3b
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-05-06 22:54:30 +0800

    indent

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,93 +1,93 @@
 #
 class ccgcompute () inherits ccgcompute::params {
 
-    class {'openstack':}
+  class {'openstack':}
 
-    file {'/etc/network/interfaces':
-      content => template('ccgcompute/interfaces.erb'),
-    }
+  file {'/etc/network/interfaces':
+    content => template('ccgcompute/interfaces.erb'),
+  }
 
-    package { $ccgcompute::absent_packages:
-      ensure  => absent,
-      require => Class['openstack']
-    }
+  package { $ccgcompute::absent_packages:
+    ensure  => absent,
+    require => Class['openstack']
+  }
 
-    package { $ccgcompute::packages:
-      ensure  => present,
-      require => Class['openstack']
-    }
+  package { $ccgcompute::packages:
+    ensure  => present,
+    require => Class['openstack']
+  }
 
-    file { '/usr/local/bin/ccgcompute-setup.sh':
-      ensure  => present,
-      owner   => root,
-      group   => root,
-      mode    => '0755',
-      content => template('ccgcompute/ccgcompute-setup.sh.erb'),
-    }
+  file { '/usr/local/bin/ccgcompute-setup.sh':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    content => template('ccgcompute/ccgcompute-setup.sh.erb'),
+  }
 
-    file { '/usr/local/bin/ccgcompute-nework.sh':
+  file { '/usr/local/bin/ccgcompute-nework.sh':
       ensure  => present,
       owner   => root,
       group   => root,
       mode    => '0755',
       content => template('ccgcompute/ccgcompute-network.sh.erb'),
-    }
+  }
 
-    exec {'system initial setup':
-      command  => '/usr/local/bin/ccgcompute-setup.sh',
-      provider => shell,
-      require  => File['/usr/local/bin/ccgcompute-setup.sh']
-    }
+  exec {'system initial setup':
+    command  => '/usr/local/bin/ccgcompute-setup.sh',
+    provider => shell,
+    require  => File['/usr/local/bin/ccgcompute-setup.sh']
+  }
 
-    exec {'system initial network setup':
-      command  => '/usr/local/bin/ccgcompute-nework.sh',
-      provider => shell,
-      require  => File['/usr/local/bin/ccgcompute-setup.sh']
-    }
+  exec {'system initial network setup':
+    command  => '/usr/local/bin/ccgcompute-nework.sh',
+    provider => shell,
+    require  => File['/usr/local/bin/ccgcompute-setup.sh']
+  }
 
-    exec { '/usr/sbin/dpkg-statoverride --update --add root root 0644 /boot/vmlinuz-`/bin/uname -r` || exit 0': }
+  exec { '/usr/sbin/dpkg-statoverride --update --add root root 0644 /boot/vmlinuz-`/bin/uname -r` || exit 0': }
 
-    file { '/etc/nova/api-paste.ini':
-      ensure  => present,
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      content => template('ccgcompute/api-paste.ini.erb'),
-      require => Package[$ccgcompute::packages],
-      notify  => Service['nova-compute'],
-    }
+  file { '/etc/nova/api-paste.ini':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('ccgcompute/api-paste.ini.erb'),
+    require => Package[$ccgcompute::packages],
+    notify  => Service['nova-compute'],
+  }
 
-    file { '/etc/nova/nova.conf':
-      ensure  => present,
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      content => template('ccgcompute/nova.conf.erb'),
-      require => Package[$ccgcompute::packages],
-      notify  => Service['nova-compute'],
-    }
+  file { '/etc/nova/nova.conf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('ccgcompute/nova.conf.erb'),
+    require => Package[$ccgcompute::packages],
+    notify  => Service['nova-compute'],
+  }
 
-    service { 'nova-compute':
-      ensure    => running,
-      enable    => true,
-      provider  => upstart,
-      require   => Package[$ccgcompute::packages],
-      subscribe => File['/etc/nova/nova.conf'],
-    }
+  service { 'nova-compute':
+    ensure    => running,
+    enable    => true,
+    provider  => upstart,
+    require   => Package[$ccgcompute::packages],
+    subscribe => File['/etc/nova/nova.conf'],
+  }
 
-    service { 'nova-network':
-      ensure    => running,
-      enable    => true,
-      provider  => upstart,
-      require   => Package[$ccgcompute::packages],
-      subscribe => File['/etc/nova/nova.conf'],
-    }
+  service { 'nova-network':
+    ensure    => running,
+    enable    => true,
+    provider  => upstart,
+    require   => Package[$ccgcompute::packages],
+    subscribe => File['/etc/nova/nova.conf'],
+  }
 
-    service { 'nova-api-metadata':
-      ensure    => running,
-      enable    => true,
-      provider  => upstart,
-      require   => Package[$ccgcompute::packages],
-      subscribe => File['/etc/nova/nova.conf'],
-    }
+  service { 'nova-api-metadata':
+    ensure    => running,
+    enable    => true,
+    provider  => upstart,
+    require   => Package[$ccgcompute::packages],
+    subscribe => File['/etc/nova/nova.conf'],
+  }
 }
